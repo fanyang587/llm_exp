5280a206aeb89218bf4b44cdd0e03e3b5bc374e7
fanyang587 <fanyang_uestc@hotmail.com>
2025-05-20 20:59:41 +0400

    add

--- story/comic_gen.py
+++ story/comic_gen.py
@@ -413,12 +413,12 @@ num_steps = 50
 general_prompt = "Mickey Mouse, wearing red shorts and yellow shoes"
 negative_prompt = "naked, deformed, bad anatomy, disfigured, poorly drawn face, mutation, extra limb, ugly, disgusting, poorly drawn hands, missing limb, floating limbs, disconnected limbs, blurry, watermarks, oversaturated, distorted hands, amputation"
 prompt_array = [
-    "wakes up in his cozy bed at home",
-    "enjoys a cheerful breakfast with Pluto",
-    "walks through the sunny forest on the way to Minnie’s house",
-    "helps Goofy fix a toy in the workshop",
-    "plays catch with Donald Duck in the backyard",
-    "relaxes on the couch reading a comic book in the evening"
+    "Bugs Bunny and Mickey stand excitedly at the entrance of Disneyland.",
+    "They ride a rollercoaster, Bugs throws his carrot in the air.",
+    "Bugs gets stuck inside a teacup ride, dizzy-eyed.",
+    "Mickey pulls Bugs out of a popcorn machine, both covered in popcorn.",
+    "They meet a crowd of fans asking for photos.",
+    "The two friends watch the fireworks, smiling under the night sky."
 ]
 
 def apply_style_positive(style_name: str, positive: str):
@@ -428,7 +428,7 @@ def apply_style(style_name: str, positives: list, negative: str = ""):
     p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
     return [p.replace("{prompt}", positive) for positive in positives], n + ' ' + negative
 ### Set the generated Style
-style_name = "Pixar/Disney Character"
+style_name = "Disney comic"
 setup_seed(seed)
 generator = torch.Generator(device="cuda").manual_seed(seed)
 prompts = [general_prompt+","+prompt for prompt in prompt_array]
