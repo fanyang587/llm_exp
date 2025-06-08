f466b974ea519ea95d7972a31e12d3571799f754
fanyang587 <fanyang_uestc@hotmail.com>
2025-06-08 20:27:34 +0400

    story generatiom

---
+++ story_v2/gen_story_v1.py
@@ -0,0 +1,92 @@
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
+os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"  # Specify GPUs to use
+SCENARIO = """
+Characters: Peter is a tall guy with blond hair. Steven is a small guy with black hair.
+Peter and Steven walk together in new york when aliens attack the city. They are afraid and try to run for their lives. The army arrive and save them.
+"""
+template_prompt = """
+You are a cartoon creator.
+
+You will be given a short scenario, you must split it in 6 parts.
+Each part will be a different cartoon panel.
+For each cartoon panel, you will write a description of it with:
+ - the characters in the panel, they must be described precisely each time
+ - the background of the panel
+The description should be only word or group of word delimited by a comma, no sentence.
+Always use the characters descriptions instead of their name in the cartoon panel description.
+You can not use the same description twice.
+You will also write the text of the panel.
+The text should not be more than 2 small sentences.
+Each sentence should start by the character name
+
+Example input:
+Characters: Adrien is a guy with blond hair wearing glasses. Vincent is a guy with black hair wearing a hat.
+Adrien and vincent want to start a new product, and they create it in one night before presenting it to the board.
+
+Example output:
+
+# Panel 1
+description: 2 guys, a blond hair guy wearing glasses, a dark hair guy wearing hat, sitting at the office, with computers
+text:
+```
+Vincent: I think Generative AI are the future of the company.
+Adrien: Let's create a new product with it.
+```
+# end
+
+Short Scenario:
+{:}
+
+Split the scenario in 6 parts:
+"
+"""
+#####
+STYLE = "american comic, colored"
+#####
+# prepare the model input
+model_name = "Qwen/Qwen3-235B-A22B"
+prompt = template_prompt.format(SCENARIO)
+# load the tokenizer and the model
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
+)
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+# conduct text completion
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=32768
+)
+output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
+
+# parsing thinking content
+try:
+    # rindex finding 151668 (</think>)
+    index = len(output_ids) - output_ids[::-1].index(151668)
+except ValueError:
+    index = 0
+
+thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+print("thinking content:", thinking_content)
+print("content:", content)
